027b07f673c90f457c7eb61df4419edbf7fb37a7
masood <masooodm@yahoo.com>
2021-10-24 11:49:42 +0330

    simple change

--- 18. Dog Research - 2/app.js
+++ 18. Dog Research - 2/app.js
@@ -16,7 +16,6 @@ Test data:
 § Data 1: [5, 2, 4, 1, 15, 8, 3]
 § Data 2: [16, 6, 10, 5, 6, 1, 4]
 */
-
 const calcAverageHuman = (ages) => {
   const humanAge = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
   console.log(humanAge);
